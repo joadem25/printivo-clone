f77e6b32fe09594bfef30db7d7230eb903b1ce6e
Joshua Ade-Omowaye <113337378+joadem25@users.noreply.github.com>
2023-02-24 19:29:03 +0100

    Add files via upload

---
+++ printivo.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var pix = htmBoss.scrollTop;
+    if (pix > 50) {
+        topNav.style.backgroundColor = "red";
+    } else {
+        topNav.style.backgroundColor = "";
+    }
+    }
